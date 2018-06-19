1cf935c0a0b403dfdfed898bc8aefbfe0ead1685
gsoules <gsoules@avantlogic.com>
2018-06-19 15:32:24 -0400

    Initial commit

---
+++ AvantDplaPlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+class AvantDplaPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    protected $_filters = array(
+        'oai_pmh_append_record',
+        'oai_pmh_repository_metadata_formats'
+    );
+
+    public function filterOaiPmhAppendRecord($item)
+    {
+        $restrictions = $item->getElementTexts('Item Type Metadata', 'Restrictions');
+        if (count($restrictions) > 0)
+        {
+            $item = null;
+        }
+
+        return $item;
+    }
+
+    public function filterOaiPmhRepositoryMetadataFormats($class)
+    {
+        $class['swhpl'] = array(
+            'class' => 'SwhplOaiDc',
+            'namespace' => OaiPmhRepository_Metadata_OaiDc::METADATA_NAMESPACE,
+            'schema' => OaiPmhRepository_Metadata_OaiDc::METADATA_SCHEMA);
+
+        return $class;
+    }
+}
